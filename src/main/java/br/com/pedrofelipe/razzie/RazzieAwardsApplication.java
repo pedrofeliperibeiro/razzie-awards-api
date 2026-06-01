b687a7e81c81c6817e1e8975cf49508fed60b9ee
Pedro Felipe De Oliveira Ribeiro <pedrofelipeo2378@gmail.com>
2026-06-01 19:41:06 -0300

    chore: scaffold Spring Boot 3 project with Java 21 and maven wrapper

---
+++ src/main/java/br/com/pedrofelipe/razzie/RazzieAwardsApplication.java
@@ -0,0 +1,12 @@
+package br.com.pedrofelipe.razzie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RazzieAwardsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RazzieAwardsApplication.class, args);
+    }
+}
